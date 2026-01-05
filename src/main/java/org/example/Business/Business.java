b4818c103e2e003e4cae75602bcd717b03f2b5eb
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-01-05 15:56:21 +0100

    creation du fichier business qui ressemble tt les methode de chaque methode de chaque classe

--- src/main/java/org/example/Business/Business.java
+++ src/main/java/org/example/Business/Business.java
@@ -1,4 +1,46 @@
 package org.example.Business;
 
-public class business {
+import org.example.Class.Formation;
+import org.example.ConnexionBDD;
+import org.example.Dao.FormationDao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Business {
+    static FormationDao formationDao = new FormationDao();
+
+    public static void test_connection() {
+            try {
+                Connection conn = ConnexionBDD.getConnection();
+                System.out.println("Connecté");
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println("Erreur : " + e.getMessage());
+            }
+    }
+
+    public static void getAllFormation() {
+        try {
+            List<Formation> formations = formationDao.findAll();
+
+            for (Formation f : formations) {
+                System.out.println(f);
+            }
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la recuperation formation");
+            e.printStackTrace();
+        }
+    }
+
+    public static void getFormationById(long id) {
+        try {
+            Formation formation = formationDao.findById(id);
+            System.out.println(formation);
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la recuperation d'une formation");
+            e.printStackTrace();
+        }
+    }
 }
