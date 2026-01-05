c05a16bc9c71ffd9420c36e72f46e77b74ab4433
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-01-05 09:24:08 +0100

    ajout du mcd du projet et modification du fichier connexion a la BDD

--- src/main/java/org/example/ConnexionBDD.java
+++ src/main/java/org/example/ConnexionBDD.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConnexionBDD {
 
-    private static final String URL = "jdbc:mariadb://localhost:3308/banque";
-    private static final String USER = "root";
-    private static final String PASSWORD = "fms";
+    private static final String URL = "jdbc:mariadb://localhost:3308/formation";
+    private static final String USER = "formation_user";
+    private static final String PASSWORD = "formation2025";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
