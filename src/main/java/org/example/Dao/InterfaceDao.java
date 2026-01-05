201f1218587cf98c1766123e5331c33bb786769b
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-01-05 15:54:59 +0100

    creation de linterface methode entre chaque dao

--- src/main/java/org/example/Dao/InterfaceDao.java
+++ src/main/java/org/example/Dao/InterfaceDao.java
@@ -1,4 +1,11 @@
 package org.example.Dao;
 
-public interface InterfaceDao {
+import java.util.List;
+
+public interface InterfaceDao<T> {
+    void save(T entity);
+    T findById(Long id);
+    List<T> findAll();
+    void update(T entity);
+    void delete(Long id);
 }
