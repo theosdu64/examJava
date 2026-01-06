2c35d3861cd29213921c84fb825a29913e9217f1
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-01-06 13:48:54 +0100

    classe client cree

---
+++ src/main/java/org/example/Class/Client.java
@@ -0,0 +1,80 @@
+package org.example.Class;
+
+public class Client {
+
+    private long id_client;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String adresse;
+    private String telephone;
+
+    public Client(long id_client, String telephone, String adresse, String email, String prenom, String nom) {
+        this.id_client = id_client;
+        this.telephone = telephone;
+        this.adresse = adresse;
+        this.email = email;
+        this.prenom = prenom;
+        this.nom = nom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public long getId_client() {
+        return id_client;
+    }
+
+    public void setId_client(long id_client) {
+        this.id_client = id_client;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id_client=" + id_client +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
+}
