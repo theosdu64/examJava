07ffa41618ed2ef3120fdadfbb55cc82e1158117
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2026-01-06 13:49:28 +0100

    ajout de la methode getclientbyid

--- src/main/java/org/example/Dao/ClientDao.java
+++ src/main/java/org/example/Dao/ClientDao.java
@@ -1,4 +1,48 @@
 package org.example.Dao;
 
-public class ClientDao {
+import org.example.Class.Client;
+import org.example.Dao.Helper.DaoHelper;
+import org.example.Dao.Interface.InterfaceDao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ClientDao extends DaoHelper implements InterfaceDao<Client> {
+
+    @Override
+    public void save(Client entity) {
+        throw new UnsupportedOperationException("non implémenté");
+    }
+
+    @Override
+    public Client findById(Long id) {
+        String sql = "SELECT * FROM client WHERE id_client=?";
+        return DaoHelper.executeQuerySingle(sql, this::mapRow,id);    }
+
+    @Override
+    public List<Client> findAll() {
+        throw new UnsupportedOperationException("non implémenté");
+    }
+
+    @Override
+    public void update(Client entity) {
+        throw new UnsupportedOperationException("non implémenté");
+    }
+
+    @Override
+    public void delete(Long id) {
+        throw new UnsupportedOperationException("non implémenté");
+    }
+
+    private Client mapRow(ResultSet  rs) throws SQLException {
+        return new Client(
+                rs.getLong("id_client"),
+                rs.getString("telephone"),
+                rs.getString("adresse"),
+                rs.getString("email"),
+                rs.getString("prenom"),
+                rs.getString("nom")
+                );
+    }
 }
